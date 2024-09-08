f3708376f69467e1a3ea99f611960bb2e8fa7fa0
Maksim Anikeev <maxim.anikeev0505@gmail.com>
2024-09-09 03:44:45 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function maryImages() {
+    document.getElementById('image1').src='images/mary1.jpg';
+    document.getElementById('image2').src='images/mary3.jpg';
+    document.getElementById('imageMain').src='images/mainmary.jpg';
+    document.getElementById('btnimg1').style.textDecoration = 'underline';
+    document.getElementById('btnimg2').style.textDecoration = 'none';
+    document.getElementById('btnimg3').style.textDecoration = 'none';
+}
+
+function lsImages() {
+    document.getElementById('image1').src='images/ls1.jpg';
+    document.getElementById('image2').src='images/ls2.jpg';
+    document.getElementById('imageMain').src='images/ls3.jpg';
+    document.getElementById('btnimg1').style.textDecoration = 'none';
+    document.getElementById('btnimg2').style.textDecoration = 'underline';
+    document.getElementById('btnimg3').style.textDecoration = 'none';
+}
+
+function portImages() {
+    document.getElementById('image1').src='images/review1.jpg';
+    document.getElementById('image2').src='images/main.jpg';
+    document.getElementById('imageMain').src='images/aboutme.jpg';
+    document.getElementById('btnimg1').style.textDecoration = 'none';
+    document.getElementById('btnimg2').style.textDecoration = 'none';
+    document.getElementById('btnimg3').style.textDecoration = 'underline';
+}
